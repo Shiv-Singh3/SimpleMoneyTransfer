f990066eb8dd9782d693a74eedc72f792028e469
shivendra <Shivendra.B.Singh@walmart.com>
2020-02-06 20:34:11 +0530

    injected dependency for account create api

--- src/main/java/simpleMoneyTransfer/constants/CommonConstants.java
+++ src/main/java/simpleMoneyTransfer/constants/CommonConstants.java
@@ -17,6 +17,7 @@ public class CommonConstants {
     public static final int HTTP_STATUS_NO_CONTENT = 204;
     public static final int HTTP_STATUS_CREATED = 201;
     public static final int HTTP_STATUS_CONFLICT = 409;
+    public static final int HTTP_STATUS_NOT_FOUND = 404;
     public static final int HTTP_STATUS_BAD_REQUEST = 400;
     public static final int HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
 }
--- src/main/java/simpleMoneyTransfer/webServices/AccountWebServices.java
+++ src/main/java/simpleMoneyTransfer/webServices/AccountWebServices.java
@@ -1,9 +1,12 @@
 package simpleMoneyTransfer.webServices;
 
+import javax.inject.Named;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
+import com.google.inject.Inject;
 import io.swagger.annotations.*;
+import simpleMoneyTransfer.constants.CommonConstants;
 import simpleMoneyTransfer.manager.AccountWebServiceManager;
 import simpleMoneyTransfer.webServices.dto.AccountDTO;
 import simpleMoneyTransfer.webServices.validation.ValidCreateAccountJson;
@@ -18,23 +21,32 @@ public class AccountWebServices {
 
     private ConcurrentHashMap<Integer, AccountDTO> accounts;
 
+    @Inject
+    public AccountWebServices(@Named("accountWebServiceManager") AccountWebServiceManager accountWebServiceManager) {
+        this.accountWebServiceManager = accountWebServiceManager;
+        this.accounts = new ConcurrentHashMap<>();
+    }
+
     @POST
     @Path("/create")
     @ApiOperation(value = "Creates an Account", response = AccountWebServices.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successfully created an account", response = AccountWebServices.class),
-            @ApiResponse(code = 404, message = "Not Found", response = AccountWebServices.class),
-            @ApiResponse(code = 500, message = "Error while processing the request",response = AccountWebServices.class)
+            @ApiResponse(code = CommonConstants.HTTP_STATUS_CREATED,
+                    message = "Successfully created an account", response = AccountWebServices.class),
+            @ApiResponse(code = CommonConstants.HTTP_STATUS_NOT_FOUND,
+                    message = "Not Found", response = AccountWebServices.class),
+            @ApiResponse(code = CommonConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
+                    message = "Error while processing the request",response = AccountWebServices.class),
+            @ApiResponse(code = CommonConstants.HTTP_STATUS_BAD_REQUEST,
+                    message = "Bad Request", response = AccountWebServices.class)
     })
     public Response createAccount(@ValidCreateAccountJson String inputString,
                   @ApiParam(name = "Accept-Language", value = "The value to be passed as header parameter",
                           required = true, defaultValue = "en-US")
                   @HeaderParam("Accept-Language") @ValidLanguageCode String languageCode) {
-        accountWebServiceManager = new AccountWebServiceManager();
-        accounts = new ConcurrentHashMap<>();
         AccountDTO accountDTO = accountWebServiceManager.parseAccountJson(inputString);
         accountWebServiceManager.createAccount(accountDTO, accounts);
-        return Response.status(201).entity(accounts.toString()).build();
+        return Response.status(Response.Status.CREATED).entity(accounts.toString()).build();
     }
 }
 
